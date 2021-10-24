11d16cfb1e81c6a729eba6b3119dc154d2da7679
Josh Bruce <josh@8fold.pro>
2021-10-23 19:09:54 -0500

    Update MarkdownBaselineTest.php

--- tests/MarkdownBaselineTest.php
+++ tests/MarkdownBaselineTest.php
@@ -31,7 +31,7 @@ test('Is performant and small', function() {
     $elapsed = $endMs - $startMs;
     $ms      = $elapsed/1e+6;
 
-    expect($ms)->toBeLessThan(1.88);
+    expect($ms)->toBeLessThan(2.08);
 
     $used = $endMem - $startMem;
     $kb   = round($used/1024.2);
