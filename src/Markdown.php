44c4b4bb687b56685f8dff876f58f265bc4eaf1c
Josh Bruce <josh@8fold.pro>
2021-11-13 14:37:02 -0600

    trial of accessible heading permalinks

--- src/Markdown.php
+++ src/Markdown.php
@@ -9,6 +9,7 @@ use Eightfold\Markdown\FluentCommonMark;
 use League\CommonMark\Extension\FrontMatter\FrontMatterExtension;
 
 use Eightfold\CommonMarkAbbreviations\AbbreviationExtension as Abbreviations;
+use Eightfold\CommonMarkAccessibleHeadingPermalink as HeadingPermalink;
 
 class Markdown extends FluentCommonMark
 {
@@ -73,5 +74,10 @@ class Markdown extends FluentCommonMark
     public function abbreviations(): Markdown
     {
         return $this->addExtension(new Abbreviations());
+    }
+
+    public function accessibleHeadingPermalinks(): Markdown
+    {
+        return $this->addExtension(new HeadingPermalink());
     }
 }
