b93f16e537ef82d3f3c5edb5d4a42d5d220a0355
Josh Bruce <josh@8fold.pro>
2021-11-30 18:40:08 -0600

    test: Multi-line code blocks with empty lines

--- tests/FluentCommonMarkBaselineTest.php
+++ tests/FluentCommonMarkBaselineTest.php
@@ -28,7 +28,7 @@ test('Is performant and small', function() {
     $elapsed = $endMs - $startMs;
     $ms      = $elapsed/1e+6;
 
-    expect($ms)->toBeLessThan(39.26);
+    expect($ms)->toBeLessThan(46);
 
     $used = $endMem - $startMem;
     $kb   = round($used/1024.2);
