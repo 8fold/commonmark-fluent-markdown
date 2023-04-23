e3e76fc46c9da06f1947981a11e0cca84c053cbe
Josh Bruce <josh@8fold.pro>
2023-04-23 17:06:13 -0400

    update: Tests to use native League Commonmark for now

--- tests/FluentCommonMarkBaselineTest.php
+++ tests/FluentCommonMarkBaselineTest.php
@@ -10,8 +10,51 @@ use Eightfold\Markdown\FluentCommonMark;
 use League\CommonMark\Environment\Environment as Environment;
 use League\Config\ConfigurationInterface as ConfigurationInterface;
 
+// use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\Attributes\AttributesExtension;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\MarkdownConverter;
+use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
+
 class FluentCommonMarkBaselineTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function can_use_attributes(): void // phpcs:ignore
+    {
+        $markdown = <<<md
+        ![this is an image](/path/to/image){width="200px", height="auto"}
+        md;
+
+        $expected = <<<html
+        <p><img src="/path/to/image" alt="this is an image" width="200px" height="auto" /></p>
+        html;
+
+        // Define your configuration, if needed
+        $config = [];
+
+        // Configure the Environment with all the CommonMark parsers/renderers
+        $environment = new Environment($config);
+        $environment->addExtension(new CommonMarkCoreExtension());
+
+        // Add this extension
+        $environment->addExtension(new AttributesExtension());
+
+        // Instantiate the converter engine and start converting some Markdown!
+        $converter = new MarkdownConverter($environment);
+        $result = $converter->convert($markdown)->getContent();
+
+        // $result = FluentCommonMark::create()->withEnvironment($environment)->commonMarkCore()
+            // ->attributes()->convertToHtml($markdown)->getContent();
+
+        $this->assertSame(
+            $expected,
+            $result
+            // '<p><img src="/path/to/image" alt="this is an image" />{width=&quot;200px&quot;, height=&quot;auto&quot;}</p>
+        );
+    }
+
     /**
      * @test
      */
